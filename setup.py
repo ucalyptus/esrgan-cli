f7a93557fe5dafdcd2162652f52a9580ea959b99
Sayantan Das <santaclaus30011998@gmail.com>
2023-05-18 03:41:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='esrgan-cli',
-    version='1.0.0',
+    version='1.1.0',
     author='Sayantan Das',
     author_email='sdas.codes@gmail.com',
     description='ESRGAN CLI Tool',
